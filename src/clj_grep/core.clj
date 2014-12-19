f9fff60015107c72511f7b4455833f6c98aff794
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-12-19 21:55:24 +0100

    Implement grep

--- src/clj_grep/core.clj
+++ src/clj_grep/core.clj
@@ -1,7 +1,34 @@
 (ns clj-grep.core
   (:gen-class))
 
+(defn file-path [file]
+  (.getPath file))
+
+(defn file-has-content? [file content]
+  (let [pattern (re-pattern (str "(?x).*" content ".*"))
+        file-content (slurp file)]
+    (re-find pattern file-content)))
+
+(defn recursive-ls [path]
+  (file-seq (clojure.java.io/file path)))
+
+(defn file? [file-or-directory]
+  (.isFile file-or-directory))
+
+(defn find-files-with-content [path content]
+  (->> (recursive-ls path)
+       (filter file?)
+       (filter #(file-has-content? % content))))
+
+(defn print-files-with-content [path content]
+  (let [files (find-files-with-content path content)]
+    (if (empty? files)
+      (println "Nothing found")
+      (do
+        (println "Found in:")
+        (doseq [file files]
+          (println (file-path file)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (apply print-files-with-content args))
